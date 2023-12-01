4eecaf58ce77ca8650c2b543b65881ef9a16440e
tempestpulse <bulsar@tutanota.com>
2023-12-01 14:41:39 +0100

    added post-detail

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from core.views import PostDetail
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('post/<int:pk>/', PostDetail.as_view(), name='post-detail')
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
