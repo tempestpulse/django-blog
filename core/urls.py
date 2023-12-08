93d5889a3a9ef2bb11e703e4c30c4b94c1f7dc0f
tempestpulse <bulsar@tutanota.com>
2023-12-08 13:47:31 +0100

    added searching by categories

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('post/<int:pk>/', PostDetail.as_view(), name='post-detail')
+    path('post/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('search/', views.search, name='search'),
+    path('category/<int:category_id>', views.category_browse, name='category-browse')
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Post, Category
 
@@ -25,5 +25,33 @@ class PostDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post_list'] = Post.objects.all()
+        context['category_list'] = Category.objects.all()
         return context
+
+
+def search(request):
+    category_list = Category.objects.all()
+    if request.method == 'GET':
+        search_input = request.GET.get('q')
+        post_list = Post.objects.filter(title__icontains=search_input)
+
+        context = {
+            'category_list': category_list,
+            'search_input': search_input,
+            'post_list': post_list,
+        }
+        return render(request, 'core/search_result.html', context)
+
+
+def category_browse(request, category_id):
+    category_list = Category.objects.all()
+    category_selected = get_object_or_404(Category, id=category_id)
+    post_list = Post.objects.filter(category=category_selected)
+
+    context = {
+        'post_list': post_list,
+        'category_list': category_list
+    }
+
+    return render(request, 'core/category_result.html', context)
+
