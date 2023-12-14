adb76c98b26e30dc62516676214a50acf7d01ae3
tempestpulse <bulsar@tutanota.com>
2023-12-15 00:16:46 +0100

    added ckeditor and Comment model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 User = get_user_model()
@@ -16,8 +17,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     overview = models.CharField(max_length=60, null=True, blank=True)
-    content = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    content = RichTextField(blank=True, null=True)
+    category = models.ManyToManyField(Category)
     thumbnail = models.ImageField(upload_to='media/thumbnails', null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     view_count = models.IntegerField(default=0)
@@ -25,5 +26,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=800)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} -- {}'.format(self.author, self.post)
 
 
