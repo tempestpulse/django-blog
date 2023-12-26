6295c540ce0b026e8a8882f3bde22962142a1426
adrian <bulsar@tutanota.com>
2023-12-26 23:05:01 +0100

    added update, delete views for posts and delete view for comments

--- core/urls.py
+++ core/urls.py
@@ -7,11 +7,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.log_in, name='login'),
-    path('logout', views.log_out, name='logout'),
+    path('logout/', views.log_out, name='logout'),
     path('register/', views.register, name='register'),
     path('post/<int:pk>/', views.post_detail, name='post-detail'),
     path('search/', views.search, name='search'),
     path('category/<int:category_id>', views.category_browse, name='category-browse'),
-    path('add_post/', views.add_post, name='add-post')
-
-              ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('add_post/', views.add_post, name='add-post'),
+    path('post_update/<int:pk>/', views.post_update, name='post-update'),
+    path('post_delete/<int:pk>/', views.post_delete, name='post-delete'),
+    path('comment_delete/<int:pk>/', views.comment_delete, name='comment-delete'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -3,10 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import F
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
 from .forms import PostForm, CommentForm, RegisterForm
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 
 def home(request):
@@ -74,6 +76,36 @@ def add_post(request):
             return redirect('post-detail', pk=new_post.id)
 
     return render(request, 'core/add_post.html', {'form': form})
+
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user != post.author:
+        raise Http404('Unable to update post, you are not the author')
+
+    form = PostForm(request.POST or None, request.FILES or None, instance=post)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+            return redirect(reverse('post-detail', kwargs={'pk': form.instance.id}))
+    return render(request, 'core/post_update.html', {'form': form})
+
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user != post.author:
+        raise Http404('Unable to delete this post, you are not the author')
+    post.delete()
+    return redirect('home')
+
+
+def comment_delete(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.user != comment.author:
+        raise Http404('Unable to delete this comment, you are not the creator')
+    comment.delete()
+    return redirect(reverse('post-detail', kwargs={'pk': comment.post_id}))
 
 
 def log_in(request):
